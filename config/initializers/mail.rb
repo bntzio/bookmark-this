04da2c6a47279355be737e0c60fe62421fefcdb8
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-03 14:37:19 -0600

    Installed and configured Mailgun

---
+++ config/initializers/mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  port:              587, 
+  address:           'smtp.mailgun.org',
+  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+  password:          ENV['MAILGUN_SMTP_PASSWORD'],
+  domain:            'appfc266c436eb04ebaae05a3f3f8ad7e49.mailgun.org',
+  authentication:    :plain,
+  content_type:      'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+# Makes debugging *way* easier.
+ActionMailer::Base.raise_delivery_errors = true
