0125a1eb330f1f393bd0cf196f3844f7f2037451
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-03 21:44:09 -0600

    Added logic to incoming controller, first test

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,2 +1,14 @@
 class IncomingController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.create(title: params[:subject], user_id: @user.id)
+    @url = params["body-plain"]
+    @bookmark = Bookmark.create(url: @url, topic_id: @topic.id)
+
+    puts "#{@user}, #{@topic}, #{@url}"
+
+    head 200
+  end
 end
