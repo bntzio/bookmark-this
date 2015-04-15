130f923bdc3448084299dde87bfa95675ee7a6d8
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-15 09:59:01 -0700

    Created CRUD actions for topics and modified styles

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,8 +10,57 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+    @bookmark = Bookmark.new
   end
 
+  def create
+    @topic = current_user.topics.new(topic_params)
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else 
+      flash[:error] = "There was an error saving the topic. Please try again."
+      render :new
+    end
+  end
+
+
   def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    name = @topic.title
+
+    if @topic.update_attributes(topic_params)
+      new_name = @topic.title 
+      flash[:notice] = "\"#{name}\" is now \"#{new_name}\"."
+      redirect_to @topic
+    else
+      flash[:error] = "Error updating topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    name = @topic.title
+
+    if @topic.destroy
+      flash[:notice] = "\"#{name}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic. Please try again."
+      render :show
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title)
   end
 end
