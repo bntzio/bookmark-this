22714374b694dd77650e359a6f2e3cdb1e1e3728
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-27 15:12:14 -0500

    Added validation to topics, updated guidelines for topics and fixed path of a link

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ActiveRecord::Base
   has_many :bookmarks, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 4, maximum: 32 }, presence: true
 end
