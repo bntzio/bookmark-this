904a96375919df87763202110becc24484ad78c5
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-27 19:22:52 -0500

    Created custom 404 error page - completed

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module BookmarkThis
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    require Rails.root.join("lib/custom_public_exceptions")
+    config.exceptions_app = CustomPublicExceptions.new(Rails.public_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
   get 'users/profile'
 
   resources :topics do
