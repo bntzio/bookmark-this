7e654661cc17609db37504f4b221f44ff28b6c41
Enrique Benitez <enriquebenitezjr@gmail.com>
2015-04-29 17:32:33 -0500

    Deleted call to Embedly API on Incoming Controller, as it is useless in there

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,15 +1,6 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  require 'embedly'
-  require 'json'
-  
-  def display(url)
-    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY']
-    obj = embedly_api.oembed :url => url
-    obj.first
-  end
-
   def create
     @user = User.find_by(email: params[:sender])
     @topic = Topic.find_by(title: params[:subject])
